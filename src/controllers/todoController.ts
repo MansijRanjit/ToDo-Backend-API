956adc5d0ae57233bc3b44445b2edf7b1b1a2397
Mansij <mansijranjit@gmail.com>
2024-01-03 10:03:06 +0545

    ammend services

--- src/controllers/todoController.ts
+++ src/controllers/todoController.ts
@@ -1,11 +1,11 @@
 import tasks from "../models/todo";
 import { Request, Response } from "express";
+import * as todoService from "../services/todoServices"
 
 //Display Task
 const getTask = async (req: Request & { userId?: number }, res: Response) => {
   try {
-    const task = tasks.filter((task) => task.userId === req.userId);
-
+    const task= todoService.getTask(req);
     res.status(200).json(task);
   } catch (error) {
     res.status(500).json({ message: "Something went wrong" });
@@ -17,16 +17,8 @@ const createTask = async (
   req: Request & { userId?: number },
   res: Response
 ) => {
-  const { title } = req.body;
-
-  const newTask = {
-    id: tasks.length + 1,
-    value: title,
-    completed: false,
-    userId: req.userId || 0,
-  };
   try {
-    tasks.push(newTask);
+    const newTask = todoService.createTask(req);
     res.status(201).json(newTask);
   } catch (error) {
     res.status(500).json({ message: "Something went wrong" });
@@ -35,20 +27,10 @@ const createTask = async (
 
 //Update Task
 const updateTask = (req: Request & { userId?: number }, res: Response) => {
-  const taskId = parseInt(req.params.id);
+  
   try {
-    const taskIndex = tasks.findIndex(
-      (task) => task.id == taskId && task.userId == req.userId
-    );
-
-    if (taskIndex === -1) {
-      return res.status(404).json({ message: "Task not found" });
-    }
-
-    tasks[taskIndex].completed =
-      req.body.completed || tasks[taskIndex].completed;
-
-    res.status(200).json(tasks[taskIndex]);
+    const task = todoService.updateTask(req);
+    res.status(200).json(task);
   } catch (error) {
     res.status(500).json({ message: "Something went wrong" });
   }
@@ -56,17 +38,8 @@ const updateTask = (req: Request & { userId?: number }, res: Response) => {
 
 //Delete Task
 const deleteTask = (req: Request & { userId?: number }, res: Response) => {
-  const taskId = parseInt(req.params.id);
   try {
-    const taskIndex = tasks.findIndex(
-      (task) => task.id == taskId && task.userId == req.userId
-    );
-
-    if (taskIndex === -1) {
-      return res.status(404).json({ message: "Task not found" });
-    }
-
-    const deletedTask = tasks.splice(taskIndex, 1)[0];
+    const deletedTask= todoService.deleteTask(req);
 
     res.status(200).json(deletedTask);
   } catch (error) {
--- src/services/todoServices.ts
+++ src/services/todoServices.ts
@@ -1,1 +1,57 @@
+import { Request } from "express";
+import tasks from "../models/todo";
+import NotFoundError from "../error/notFoundError";
 
+export async function getTask(req:Request &{userId? :number}){
+  const task = tasks.filter((task) => task.userId === req.userId);
+  return task;
+}
+
+export async function createTask(req:Request & {userId? : number}){
+  const { title } = req.body;
+
+  const newTask = {
+    id: tasks.length + 1,
+    value: title,
+    completed: false,
+    userId: req.userId || 0,
+  };
+  tasks.push(newTask);
+  return newTask;
+}
+
+export async function updateTask(req:Request &{userId? :number}){
+  const taskId = parseInt(req.params.id);
+
+  const taskIndex = tasks.findIndex(
+    (task) => task.id == taskId && task.userId == req.userId
+  );
+
+  if (taskIndex === -1) {
+    throw new NotFoundError("Task not found"); 
+    //return res.status(404).json({ message: "Task not found" });
+  }
+
+  tasks[taskIndex].completed =
+    req.body.completed || tasks[taskIndex].completed;
+
+  return tasks[taskIndex];
+}
+
+
+export async function deleteTask(req:Request &{userId? :number}){
+  const taskId = parseInt(req.params.id);
+
+  const taskIndex = tasks.findIndex(
+    (task) => task.id == taskId && task.userId == req.userId
+  );
+
+  if (taskIndex === -1) {
+    throw new NotFoundError("Task not found"); 
+    // return res.status(404).json({ message: "Task not found" });
+  }
+
+  const deletedTask = tasks.splice(taskIndex, 1)[0];
+
+  return deleteTask;
+}
